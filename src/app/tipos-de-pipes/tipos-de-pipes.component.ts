1d15707c826a9f12f7f750f1470424c7b4039276
getafemean <getafemean@gmail.com>
2021-05-14 19:43:30 +0200

    pipes propios

--- src/app/tipos-de-pipes/tipos-de-pipes.component.ts
+++ src/app/tipos-de-pipes/tipos-de-pipes.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiposDePipesComponent implements OnInit {
 
+  texto: string = 'ciudad de Madrid';
+  id: any = 1;
+  ahora: object = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.id = '000' + this.id;
   }
 
 }
